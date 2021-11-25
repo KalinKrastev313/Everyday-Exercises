9038e50b4ddc0bfc62d1f3a07c82a2dc257dce7f
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2021-11-25 17:27:30 +0200

    Solution Triplets of letters

---
+++ 2. Data Types and Variables/2.6. Triples of Latin Letters/main.py
@@ -0,0 +1,6 @@
+for g in range (65, 65 + 3, 1):
+    for h in range (65, 65 + 3, 1):
+        for i in range (65, 65 + 3, 1):
+            print(chr(g), end = "")
+            print(chr(h), end="")
+            print(chr(i))
