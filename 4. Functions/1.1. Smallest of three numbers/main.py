a804a4b9b9a23d72b5d256f9446c4aa6a6aa4fbf
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2021-12-18 12:08:41 +0200

    Ex1 solution

---
+++ 4. Functions/1.1. Smallest of three numbers/main.py
@@ -0,0 +1,8 @@
+def get_the_lowest (num1, num2, num3):
+    return min(num1, num2, num3)
+
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+
+print(get_the_lowest(number1, number2, number3))
