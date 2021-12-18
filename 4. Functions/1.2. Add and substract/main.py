810875366ffbcbc8bede032b498c65349a68fc2a
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2021-12-18 12:12:39 +0200

    Solution ex2

---
+++ 4. Functions/1.2. Add and substract/main.py
@@ -0,0 +1,14 @@
+def sum_numbers(num1, num2):
+    return num1 + num2
+
+def subtract (sum_result, num3):
+    return sum_result - num3
+
+def add_and_subtract (num1, num2, num3):
+    return subtract(sum_numbers(num1, num2), num3)
+
+number1 = int(input())
+number2 = int(input())
+number3 = int(input())
+
+print(add_and_subtract(number1, number2, number3))
