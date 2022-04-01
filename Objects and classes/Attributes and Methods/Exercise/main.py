2ec211a8c0224aabb100152c0192370a2db98caf
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2022-04-01 11:59:31 +0300

    Photo album almost ready

--- Objects and classes/Attributes and Methods/Exercise/main.py
+++ Objects and classes/Attributes and Methods/Exercise/main.py
@@ -14,12 +14,27 @@ class PhotoAlbum:
     def from_photos_count(cls, photos_count):
         return cls(photos_count / 4)
 
-    @classmethod
-    def free_page_slot(cls, photos):
-        for page in range(len(cls.photos):
+    @staticmethod
+    def first_page_with_free_slot(photos):
+        for page in range(len(photos)):
             if len(photos[page]) < 4:
-                return page
+                return page, len(photos[page]) + 1
+        return False, False
 
     def add_photo(self, label):
+        first_free_page, slot = PhotoAlbum.first_page_with_free_slot(self.photos)
+        if first_free_page is False:
+            return "No more free slots"
+        self.photos[first_free_page].append(label)
+        return f"{label} photo added successfully on page {first_free_page + 1} slot {slot}"
 
-                page.append(label)
+
+album = PhotoAlbum(2)
+
+print(album.add_photo("baby"))
+print(album.add_photo("first grade"))
+print(album.add_photo("eight grade"))
+print(album.add_photo("party with friends"))
+print(album.photos)
+print(album.add_photo("prom"))
+print(album.add_photo("wedding"))
