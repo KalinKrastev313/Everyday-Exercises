1771450b87573e1782871c802567275bb55dcf39
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2022-04-13 16:36:15 +0300

    Topping class

---
+++ Objects and classes/Encapsulation/Exercise/PizzaCalories/main.py
@@ -0,0 +1,27 @@
+class Topping:
+    def __init__(self, topping_type, weight):
+        self.__topping_type = topping_type
+        self.__weight = weight
+    
+    @property
+    def topping_type(self):
+        return self.__topping_type
+
+    @topping_type.setter
+    def topping_type(self, value):
+        self.__topping_type = value
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    @weight.setter
+    def weight(self, value):
+        if value < 0:
+            raise ValueError("Should be pos")
+        self.__weight = value
+
+
+t = Topping("a", -2)
+print(t.weight)
+t.weight = -3
