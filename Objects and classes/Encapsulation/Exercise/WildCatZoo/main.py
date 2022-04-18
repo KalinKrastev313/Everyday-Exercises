696d407d0f236bffa67dd667a4fb846dc7898bd2
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2022-04-18 13:53:31 +0300

    First runs of ex 1 zoo

--- Objects and classes/Encapsulation/Exercise/WildCatZoo/main.py
+++ Objects and classes/Encapsulation/Exercise/WildCatZoo/main.py
@@ -22,20 +22,20 @@ for i in range(len(animals)):
     animal = animals[i]
     price = prices[i]
     print(zoo.add_animal(animal, price))
-#
-# # Adding all workers
-# for worker in workers:
-#     print(zoo.hire_worker(worker))
-#
-# # Tending animals
-# print(zoo.tend_animals())
-#
-# # Paying keepers
-# print(zoo.pay_workers())
-#
-# # Fireing worker
-# print(zoo.fire_worker("Adam"))
-#
+
+# Adding all workers
+for worker in workers:
+    print(zoo.hire_worker(worker))
+
+# Tending animals
+print(zoo.tend_animals())
+
+# Paying keepers
+print(zoo.pay_workers())
+
+# Fireing worker
+print(zoo.fire_worker("Adam"))
+
 # # Printing statuses
 # print(zoo.animals_status())
 # print(zoo.workers_status())
--- Objects and classes/Encapsulation/Exercise/WildCatZoo/project/zoo.py
+++ Objects and classes/Encapsulation/Exercise/WildCatZoo/project/zoo.py
@@ -1,5 +1,5 @@
 class Zoo:
-    def __init__(self, animal_capacity, workers_capacity, budget, name, animals, workers):
+    def __init__(self, name, budget, animal_capacity, workers_capacity):
         self.__animal_capacity = animal_capacity
         self.__workers_capacity = workers_capacity
         self.__budget = budget
@@ -15,6 +15,38 @@ class Zoo:
         else:
             self.animals.append(animal)
             self.__budget -= price
-            return f"{self.name} the {animal.__class__.__name__} added to the zoo"
+            return f"{animal.name} the {animal.__class__.__name__} added to the zoo"
+
+    def hire_worker(self, worker):
+        if len(self.workers) < self.__workers_capacity:
+            self.workers.append(worker)
+            return f"{worker.name} the {worker.__class__.__name__} hired successfully"
+        else:
+            return "Not enough space for worker"
+
+    def fire_worker(self, worker_name):
+        try:
+            worker = [w for w in self.workers if w.name == worker_name][0]
+            self.workers.remove(worker)
+            return f"{worker_name} fired successfully"
+        except KeyError:
+            return f"There is no {worker_name} in the zoo"
+
+    def pay_workers(self):
+        salaries_sum = sum([w.salary for w in self.workers])
+        if self.__budget >= salaries_sum:
+            self.__budget -= salaries_sum
+            return f"You payed your workers. They are happy. Budget left: {self.__budget}"
+        return "You have no budget to pay your workers. They are unhappy"
+
+    def tend_animals(self):
+        animals_needs_cost = sum([a.get_needs() for a in self.animals])
+        if self.__budget <= animals_needs_cost:
+            self.__budget -= animals_needs_cost
+            return f"You tended all the animals. They are happy. Budget left: {self.budget}"
+        return "You have no budget to tend the animals. They are unhappy."
+
+    def profit(self, amount):
+        self.__budget += amount
 
 
