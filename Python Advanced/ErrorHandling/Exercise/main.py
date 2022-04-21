e581372a12b21e74ae1869049ca0ef8ba4dc4360
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2022-04-21 10:31:09 +0300

    Update main.py

--- Python Advanced/ErrorHandling/Exercise/main.py
+++ Python Advanced/ErrorHandling/Exercise/main.py
@@ -1,32 +1,76 @@
+# numbers_dictionary = {}
+#
+# line = input()
+#
+# while line != "Search":
+#     try:
+#         number_as_string = line
+#         number = int(input())
+#         numbers_dictionary[number_as_string] = number
+#     except ValueError:
+#         print("The variable number must be an integer")
+#
+# line = input()
+#
+# while line != "Remove":
+#     try:
+#         searched = line
+#         print(numbers_dictionary[searched])
+#     except KeyError:
+#         print("Number does not exist in dictionary")
+#
+#
+# line = input()
+#
+# while line != "End":
+#     try:
+#         searched = line
+#         del numbers_dictionary[searched]
+#     except KeyError:
+#         print("Number does not exist in dictionary")
+#
+# print(numbers_dictionary)
+
 numbers_dictionary = {}
 
-line = input()
-
-while line != "Search":
+while True:
+    number_as_string = input()
+    if number_as_string == "Search":
+        break
     try:
-        number_as_string = line
         number = int(input())
         numbers_dictionary[number_as_string] = number
     except ValueError:
         print("The variable number must be an integer")
 
+
 line = input()
 
 while line != "Remove":
-    try:
-        searched = line
-        print(numbers_dictionary[searched])
-    except KeyError:
-        print("Number does not exist in dictionary")
-
+    searched = line
+    print(numbers_dictionary[searched])
+    line = input()
 
 line = input()
 
 while line != "End":
-    try:
-        searched = line
-        del numbers_dictionary[searched]
-    except KeyError:
-        print("Number does not exist in dictionary")
+    searched = line
+    del numbers_dictionary[searched]
+    line = input()
 
 print(numbers_dictionary)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
