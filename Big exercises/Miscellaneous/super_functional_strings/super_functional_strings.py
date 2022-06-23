4eeb2d9f78504ddc46b3bad2a5e0a03baf72438f
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2022-06-23 21:06:05 +0300

    Create super_functional_strings.py

---
+++ Big exercises/Miscellaneous/super_functional_strings/super_functional_strings.py
@@ -0,0 +1,41 @@
+s = "aba"
+
+
+def create_substrings(s):
+    sub_strings = []
+
+    for i in range(len(s)):
+        for l in range(len(s), i, -1):
+            if not s[i:l] in sub_strings:
+                sub_strings.append(s[i:l])
+    return sub_strings
+
+
+def distinct(text):
+    dist = []
+    for char in text:
+        #for really long strings containing the whole alphabet
+        if len(dist) == 26:
+            break
+        #adding unique characters
+        if not char in dist:
+            dist.append(char)
+    return len(dist)
+
+
+def superFunctionalStrings(s):
+    sub_strings = create_substrings(s)
+    result = 0
+    divisor = (10 ** 9 + 7)
+    for text in sub_strings:
+        f = (len(text) ** distinct(text)) % divisor
+        result += f
+    return result
+
+
+print(superFunctionalStrings(s))
+
+
+
+
+
